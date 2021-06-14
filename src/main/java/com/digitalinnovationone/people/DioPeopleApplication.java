92bd5e1be9e9bd0c9b8244d294034c3ffde3acab
Paulo Patarello <paulohenriquepatarello@gmail.com>
2021-06-14 11:23:44 -0300

    first commit

---
+++ src/main/java/com/digitalinnovationone/people/DioPeopleApplication.java
@@ -0,0 +1,13 @@
+package com.digitalinnovationone.people;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioPeopleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioPeopleApplication.class, args);
+	}
+
+}
